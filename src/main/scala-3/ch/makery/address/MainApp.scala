ab403a0e3d25e9684cb3abe5b82e62d76b026f71
SpectreSoulz <cleveyxwong@gmail.com>
2025-07-22 15:54:07 +0800

    22/7/2025

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,7 +1,8 @@
 package ch.makery.address
 
 import ch.makery.address.model.Person
-import ch.makery.address.view.PersonEditDialogController
+import ch.makery.address.util.Database
+import ch.makery.address.view.{PersonEditDialogController, PersonOverviewController}
 import javafx.fxml.FXMLLoader
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
@@ -14,11 +15,13 @@ import scalafx.scene.image.Image
 import scalafx.stage.{Modality, Stage}
 
 object MainApp extends JFXApp3:
+  Database.setupDB()
 
   //Window Root Pane
   var roots: Option[scalafx.scene.layout.BorderPane] = None
 
   var cssResource = getClass.getResource("view/DarkTheme.css")
+  var personOverviewController: Option[PersonOverviewController] = None
   /**
    * The data as an observable list of Persons.
    */
@@ -27,15 +30,8 @@ object MainApp extends JFXApp3:
   /**
    * Constructor
    */
-  personData += new Person("Hans", "Muster")
-  personData += new Person("Ruth", "Mueller")
-  personData += new Person("Heinz", "Kurz")
-  personData += new Person("Cornelia", "Meier")
-  personData += new Person("Werner", "Meyer")
-  personData += new Person("Lydia", "Kunz")
-  personData += new Person("Anna", "Best")
-  personData += new Person("Stefan", "Meier")
-  personData += new Person("Martin", "Mueller")
+  //assign all person into personData array
+  personData ++= Person.getAllPersons
 
 
   override def start(): Unit =
@@ -64,6 +60,8 @@ object MainApp extends JFXApp3:
     val loader = new FXMLLoader(resource)
     loader.load()
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
+    val ctrl = loader.getController[PersonOverviewController]
+    personOverviewController = Option(ctrl)
     this.roots.get.center = roots
     
   extension (value: Int)
--- src/main/scala-3/ch/makery/address/view/PersonOverviewController.scala
+++ src/main/scala-3/ch/makery/address/view/PersonOverviewController.scala
@@ -79,7 +79,7 @@ class PersonOverviewController():
   def handleDeletePerson(action: ActionEvent): Unit =
     val selectedIndex = personTable.selectionModel().selectedIndex.value
     if (selectedIndex >= 0) then
-      MainApp.personData.remove(selectedIndex)  /**
+      personTable.items().remove(selectedIndex).delete()  /**
    * Called when the user clicks on the delete button.
    */
     else
@@ -94,8 +94,10 @@ class PersonOverviewController():
   def handleNewPerson(action: ActionEvent) =
     val person = new Person("", "")
     val okClicked = MainApp.showPersonEditDialog(person);
-    if (okClicked) then
+    if (okClicked) then {
       MainApp.personData += person
+      person.save()
+    }
 
   @FXML
   def handleEditPerson(action: ActionEvent) =
@@ -103,15 +105,17 @@ class PersonOverviewController():
     if (selectedPerson != null) then
       val okClicked = MainApp.showPersonEditDialog(selectedPerson)
 
-      if (okClicked) then showPersonDetails(Some(selectedPerson))
+      if (okClicked) then {
+        showPersonDetails(Some(selectedPerson))
+        selectedPerson.save()
 
-    else
-      // Nothing selected.
-      val alert = new Alert(Alert.AlertType.Warning):
-        initOwner(MainApp.stage)
-        title = "No Selection"
-        headerText = "No Person Selected"
-        contentText = "Please select a person in the table."
-      .showAndWait()
+      } else
+        // Nothing selected.
+        val alert = new Alert(Alert.AlertType.Warning):
+          initOwner(MainApp.stage)
+          title = "No Selection"
+          headerText = "No Person Selected"
+          contentText = "Please select a person in the table."
+        .showAndWait()
 
 
